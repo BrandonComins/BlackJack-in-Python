94fe9dd959fdbdb93c97f135b1582b7a0eae6c37
BrandonComins <brandcomin@gmail.com>
2023-04-03 20:43:40 -0700

    Make Deck Class

---
+++ Deck.py
@@ -0,0 +1,23 @@
+from random import randint
+
+class deck:
+    def __init__(self):
+        self.suits = ['♠', '♦', '♥', '♣']
+        self.numbers = (['A', '2', '3', '4', '5', '6' , '7' , '8' ,
+                     '9', '10', 'J', 'Q', 'K'])
+        self.cards = []
+        
+        for suit in self.suits:
+            for number in self.numbers:
+                self.cards.append(suit + number)
+        
+    def __str__(self):
+        return str(self.cards)
+
+    def giveCard(self):
+        return self.cards.pop(randint(0, len(self.cards) - 1))
+
+    def shuffle(self):
+        self.__init__()
+    
+
