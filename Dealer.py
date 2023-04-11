7394cd0d8f28ddd68c79ac4106398543c28b824c
BrandonComins <brandcomin@gmail.com>
2023-04-10 20:42:38 -0700

    remove usless and import change class name to have capital letter

--- Dealer.py
+++ Dealer.py
@@ -1,8 +1,12 @@
-from Player import player
-from Deck import deck
-class dealer(player):
+from Player import Player
+from Deck import Deck
+class Dealer(Player):
     
     def __init__(self):
         super().__init__("Dealer", 100000)
+    
+    def showHand(self):
+        print('Dealer hand is: [' + '■, ' + str(self.hand[1]) + ']')
+    
 
     
