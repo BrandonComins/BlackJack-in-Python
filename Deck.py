d9789dd13531aadc2224e507d599de5cbf9ebb56
BrandonComins <brandcomin@gmail.com>
2023-04-10 20:43:48 -0700

    clean

--- Deck.py
+++ Deck.py
@@ -1,6 +1,6 @@
 from random import randint
 
-class deck:
+class Deck:
     def __init__(self):
         self.suits = ['♠', '♦', '♥', '♣']
         self.numbers = (['A', '2', '3', '4', '5', '6' , '7' , '8' ,
