05eee6483f6273abd4e371757d282857f66aadd0
BrandonComins <brandcomin@gmail.com>
2023-04-10 20:44:21 -0700

    add takeTurn function

--- BlackJack.py
+++ BlackJack.py
@@ -1,6 +1,7 @@
-from Deck import deck
-from Player import player
-from CustomErrors import InvalidPlayerError
+from Deck import Deck
+from Player import Player
+from Dealer import Dealer
+from CustomErrors import *
 
 def getPlayers() -> list:
     while True:
@@ -20,10 +21,44 @@ def getPlayers() -> list:
     
     players : list = []
     for i in range(number_of_players):
-        players.append(player(i))
+        players.append(Player(i, 1000))
     
     return players
 
+
+
+def takeTurn(player : Player):
+    choice = 0
+    while (not player.bust or choice == 2):
+        choice = int(input("1. hit\n2.stay"))
+        if choice == 1:
+            player.hit()
+        elif choice == 2:
+            break
+        else:
+            print("Bad input, must be 1 or 2")
+            continue
+
+    
+
+
+def playGame(players : list, deck : Deck):
+    players.append(Dealer())
+    for player in players:
+        player.dealHand(deck)
+
+        if player.name != "Dealer":
+            repr(player)
+   
+    players[-1].showHand()
+
+
+
+
+
+
+
 if __name__ == "__main__":
-    deck_of_cards = deck()
-    getPlayers()
+    deck_of_cards = Deck()
+    playGame(getPlayers(), deck_of_cards)
+
